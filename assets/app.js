bbc974f7e7d0c1a4576e1a7eae65922eb411406a
Shivanshu-Chimaniya <ShivanshuChimaniya77@gmail.com>
2023-10-24 18:43:19 +0530

    CSS is Almost There, JS needs work still

--- assets/app.js
+++ assets/app.js
@@ -4,19 +4,45 @@ function addDiv(btn){
     parent.appendChild(prototype.cloneNode(true));
 }
 
+let mainForm = document.querySelector('.sectionHolder');
 
+let firstNameElement = mainForm.querySelector('#firstName'),
+    lastNameElement = mainForm.querySelector('#lastName'),
+    emailElement = mainForm.querySelector('#email'),
+    cityElement = mainForm.querySelector('#city'),
+    stateElement = mainForm.querySelector('#state'),
+    phoneElement = mainForm.querySelector('#phone');
+
+let resume = document.querySelector('.cv');
+
+let nameDisplayElement = resume.querySelector('#nameDisplay'),
+    placeDisplayElement = resume.querySelector("#placeDisplay"),
+    phoneDisplayElement = resume.querySelector("#phoneDisplay"),
+    emailDisplayElement = resume.querySelector("#emailDisplay");
+
+function getData(){
+    return {
+        name: firstNameElement.value + " " + lastNameElement.value,
+        place: cityElement.value + ", " + stateElement.value,
+        email : emailElement.value,
+        phone: phoneElement.value
+    }
+}
+
+function showData(userData){
+    nameDisplayElement.innerHTML = userData.name;
+    placeDisplayElement.innerHTML = userData.place;
+    emailDisplayElement.innerHTML = userData.email;
+    phoneDisplayElement.innerHTML = userData.phone;
+
+}
 
 function generateCV(){
-    console.log("hii");
-    // get values
-    let mainForm = document.querySelector('.sectionHolder');
-    let firstName = mainForm.querySelector('#firstName');
-        lastName = mainForm.querySelector('#lastName');
+    let userData = getData();
+    showData(userData);
+}
 
-    // process
 
-    // display
-    let resume = document.querySelector('.cv');
-    let nameDisplay = resume.querySelector('#name');
-    nameDisplay.innerHTML = firstName.value + " " + lastName.value;
+function printCV(){
+    window.print();
 }
